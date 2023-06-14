f63f1ebe057b274baa9cd57ed896fdc6688c99ef
McStudent01 <mcmillcy@mail.uc.edu>
2023-06-14 16:30:49 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TheaterKiosk
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("What is your current age: ");
+        age = in.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.println("You are over 21. Please drink responsibly.");
+        }
+    }
+}
